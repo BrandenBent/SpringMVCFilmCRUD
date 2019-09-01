788823f8d61be941a360e3fae088d4adc0c7f22f
BrandenBent <bent_branden@hotmail.com>
2019-08-31 19:28:21 -0600

    servlet configs and XML

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,23 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.film.dao.FilmDAOImpl;
+
 public class FilmController {
+	@Autowired
+	FilmDAOImpl dao;
+	
+	  @RequestMapping(path = "searchId.do", params = "filmId", method = RequestMethod.GET)
+	  public ModelAndView getFilmById(int filmId) {
+		dao = new FilmDAOImpl();
+	    ModelAndView mv = new ModelAndView();
+	    mv.addObject("film", dao.findFilmById(filmId));
+	    mv.setViewName("WEB-INF/result.jsp");
+	    return mv;
+	  }
 
 }
