cb24934260d347ad3ca5fe5176b0cff7868282f3
Connor Campora <connorcampora@Brucebs-MacBook-Pro.local>
2019-09-02 14:35:28 -0600

    recommiting to fix import errors

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,4 +1,4 @@
-package com.skilldistillery.film.controllers;
+package src.main.java.com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
