346056b7863f3a1098ff71a8184c2e9671beca70
BrandenBent <bent_branden@hotmail.com>
2019-09-02 13:06:13 -0600

    refresh commit

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,23 +1,36 @@
 package com.skilldistillery.film.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAOImpl;
+import com.skilldistillery.film.entities.Film;
 
+@Controller
 public class FilmController {
-	@Autowired
 	FilmDAOImpl dao;
-	
-	  @RequestMapping(path = "searchId.do", params = "filmId", method = RequestMethod.GET)
-	  public ModelAndView getFilmById(int filmId) {
-		dao = new FilmDAOImpl();
-	    ModelAndView mv = new ModelAndView();
-	    mv.addObject("film", dao.findFilmById(filmId));
-	    mv.setViewName("WEB-INF/result.jsp");
-	    return mv;
-	  }
+
+//	  @RequestMapping(path = "searchId.do", params = "search", method = RequestMethod.GET)
+//	  public ModelAndView getFilmById(int filmId) {
+//		dao = new FilmDAOImpl();
+//	    ModelAndView mv = new ModelAndView();
+//	    Film film = dao.findFilmById(filmId);
+//	    mv.addObject("film", film);
+//	    mv.setViewName("WEB-INF/result.jsp");
+//	    return mv;
+//	  }
+
+	@RequestMapping(path = "searchId.do", params = "search", method = RequestMethod.GET)
+	public ModelAndView getFilmById(int search) {
+		Film film = null;
+		film = dao.findFilmById(search);
+		ModelAndView mv = new ModelAndView();
+		System.out.println(film);
+		mv.addObject("film", film);
+		mv.setViewName("WEB-INF/result.jsp");
+		return mv;
+	}
 
 }
